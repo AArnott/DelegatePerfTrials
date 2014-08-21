cb52447da0825f8423889bb8d248f75dc4efc83e
Andrew Arnott <andrewarnott@gmail.com>
2014-08-20 21:09:08 -0600

    Add lightweight codegen approach to prove whether it's possible (not optimized yet).
    Right now, it fails with this exception.
    
    System.MethodAccessException was unhandled
      _HResult=-2146233072
      _message=Attempt by method 'SomeType.HelperMethod(System.String)' to access method 'ConsoleApplication11.Program.SomeAction(System.String)' failed.
      HResult=-2146233072
      IsTransient=false
      Message=Attempt by method 'SomeType.HelperMethod(System.String)' to access method 'ConsoleApplication11.Program.SomeAction(System.String)' failed.
      Source=test_7096872a-6f1d-49ec-95a1-7298f5fb81be
      StackTrace:
           at SomeType.HelperMethod(String )
           at ConsoleApplication11.Program.CreateLCGApproach[TArg,TReturn](Func`2 f, TArg arg) in c:\Users\Andrew\Documents\Visual Studio 2013\Projects\DelegatePerfTrials\DelegatePerfTrials\Program.cs:line 81
           at ConsoleApplication11.Program.Main(String[] args) in c:\Users\Andrew\Documents\Visual Studio 2013\Projects\DelegatePerfTrials\DelegatePerfTrials\Program.cs:line 44
           at System.AppDomain._nExecuteAssembly(RuntimeAssembly assembly, String[] args)
           at System.AppDomain.ExecuteAssembly(String assemblyFile, Evidence assemblySecurity, String[] args)
           at Microsoft.VisualStudio.HostingProcess.HostProc.RunUsersAssembly()
           at System.Threading.ThreadHelper.ThreadStart_Context(Object state)
           at System.Threading.ExecutionContext.RunInternal(ExecutionContext executionContext, ContextCallback callback, Object state, Boolean preserveSyncCtx)
           at System.Threading.ExecutionContext.Run(ExecutionContext executionContext, ContextCallback callback, Object state, Boolean preserveSyncCtx)
           at System.Threading.ExecutionContext.Run(ExecutionContext executionContext, ContextCallback callback, Object state)
           at System.Threading.ThreadHelper.ThreadStart()
      InnerException:

--- DelegatePerfTrials/Program.cs
+++ DelegatePerfTrials/Program.cs
@@ -22,6 +22,7 @@ namespace ConsoleApplication11
             var ctorTimer = new Stopwatch();
             var delegateTimer = new Stopwatch();
             var baselineTimer = new Stopwatch();
+            var lcgTimer = new Stopwatch();
 
             for (int i = 0; i < iterations; i++)
             {
@@ -38,6 +39,11 @@ namespace ConsoleApplication11
                 baselineTimer.Start();
                 result = f("World!");
                 baselineTimer.Stop();
+
+                lcgTimer.Start();
+                var fc3 = CreateLCGApproach(f, "World!");
+                result = fc3();
+                lcgTimer.Stop();
             }
 
             Console.WriteLine("Func<>.ctor: {0}", ctorTimer.ElapsedMilliseconds);
@@ -53,6 +59,27 @@ namespace ConsoleApplication11
         private static Func<string> CtorApproach(Func<string, string> f, ConstructorInfo ctor)
         {
             return (Func<string>)ctor.Invoke(new object[] { "World!", f.Method.MethodHandle.GetFunctionPointer() });
+        }
+
+        private static Func<TReturn> CreateLCGApproach<TArg, TReturn>(Func<TArg, TReturn> f, TArg arg)
+        {
+            var an = new AssemblyName("test_" + Guid.NewGuid());
+            var ab = AssemblyBuilder.DefineDynamicAssembly(an, AssemblyBuilderAccess.Run);
+            var dm = ab.DefineDynamicModule("Main");
+            var tb = dm.DefineType("SomeType");
+            var mb = tb.DefineMethod("HelperMethod", MethodAttributes.Static | MethodAttributes.Public, typeof(Func<TReturn>), new Type[] { typeof(TArg) });
+            var il = mb.GetILGenerator();
+
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldftn, f.Method);
+            il.Emit(OpCodes.Newobj, typeof(Func<TReturn>).GetConstructors().Single());
+            il.Emit(OpCodes.Ret);
+
+            var type = tb.CreateType();
+            var method = type.GetMethod(mb.Name, BindingFlags.Static | BindingFlags.Public);
+            var fastMethod = (Func<TArg, Func<TReturn>>)method.CreateDelegate(typeof(Func<TArg, Func<TReturn>>));
+            Func<TReturn> simpleWrapper = fastMethod(arg);
+            return simpleWrapper;
         }
 
         static string SomeAction(string value)
